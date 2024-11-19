da000992156fa63394790fb202dc97dc5ecf9427
Usenathi19 <usenathimanina@gmail.com>
2024-11-19 09:34:03 +0200

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "cc00242bebmsh3cab6a80e416369p13abc6jsn55ae10eacdf3",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0b363a65b807fef77294861825f718ca"; 
